5bf8fe7b07040acf5aaa0eedcf4737b317408368
justinslack <justin@nml.co.za>
2024-06-04 06:50:51 +0200

    Angular course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'home-page',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-course';
+  data = {
+    title: 'angular-course',
+  };
+
+  onLogoClicked() {
+    alert('Hello world');
+  }
 }
